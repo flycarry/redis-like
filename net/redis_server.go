ce520ccfc7ce3663041c9292a57ee410bb8f2f63
liufei <1373945731liufei@gmail.com>
2019-03-18 21:38:44 +0800

    server add

--- net/redis_server.go
+++ net/redis_server.go
@@ -1,17 +1,13 @@
 package net
 
 import (
-	"github.com/flycarry/redis-like/storage"
-	"io/ioutil"
+	"github.com/flycarry/redis-like/route"
+	"io"
 	"log"
 	"net"
-	"strconv"
 	"strings"
 )
-var data *storage.Data
-func init() {
-	data=storage.NewData()
-}
+
 const(
 	setString = iota
 	getString
@@ -35,36 +31,18 @@ func Socket_server(port string){
 }
 
 func resolve_conn(c net.Conn){
-	msg,err:=ioutil.ReadAll(c)
+	buf:=make([]byte,512)
+	l,err:=c.Read(buf)
 	if err != nil {
-		c.Close()
-		return
+		log.Println(err)
 	}
-	if len(msg)>1{
-		pars:=parsePars(msg)
-		methon,err:=strconv.Atoi(pars[0])
-		if err != nil {
-			c.Close()
-			return
-		}
-		switch methon {
-		case setString:
-			result:=data.SetString(pars[1],pars[2])
-			c.Write([]byte(strconv.Itoa(result)))
-		case getString:
-			resultStr,err:=data.GetString(pars[1])
-			if err != -1 {
-				c.Write([]byte(strconv.Itoa(err)))
-				return
-			}
-			c.Write([]byte(resultStr))
-		}
-	}
-	c.Close()
+	log.Println(string(buf[:l]))
+	io.WriteString(c,route.DoReply(string(buf[:l])))
+	defer c.Close()
 }
 
 func parsePars(msg []byte)[]string{
 	msgStr:=string(msg)
-	return strings.Split(msgStr," ")
+	return strings.Split(msgStr,"\r\n")
 
 }
--- route/route.go
+++ route/route.go
@@ -1,5 +1,42 @@
 package route
 
+import (
+	"github.com/flycarry/redis-like/storage"
+	"strings"
+)
 
+var data *storage.Data
 
+func init() {
+	data = storage.NewData()
+}
 
+func DoReply(request string) string {
+	request = strings.TrimSpace(request)
+	query := strings.Fields(request)
+	if len(query) <= 1 {
+		return "not a query"
+	}
+	method := query[0]
+	switch method {
+	case "get":
+		response, err := data.GetString(query[1])
+		if err != 0 {
+			return "err"
+		}
+		if len(query) != 2 {
+			return "parm not good"
+		}
+		return response
+	case "set":
+		err := data.SetString(query[1], query[2])
+		if err != 0 {
+			return "err"
+		}
+		if len(query) != 3 {
+			return "parm not good"
+		}
+		return "good"
+	}
+	return ""
+}
