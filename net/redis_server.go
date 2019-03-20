baf31263e4fc99f73f8e10685d4fce723dacd85a
liufei <1373945731liufei@gmail.com>
2019-03-20 09:10:36 +0800

    change

--- net/redis_server.go
+++ net/redis_server.go
@@ -5,43 +5,44 @@ import (
 	"io"
 	"log"
 	"net"
-	"strings"
 )
 
-const(
+const (
 	setString = iota
 	getString
 	rPush
 	rPop
 )
-func Socket_server(port string){
-	l,err:=net.Listen("tcp",port)
+
+func Socket_server(port string) {
+	l, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer l.Close()
 	for {
-		conn,err:=l.Accept()
+		conn, err := l.Accept()
 		if err != nil {
 			log.Println(err)
 		}
-		log.Printf("get request from %s",conn.RemoteAddr())
+		log.Printf("get request from %s", conn.RemoteAddr())
 		go resolve_conn(conn)
 	}
 }
 
-func resolve_conn(c net.Conn){
+func resolve_conn(c net.Conn) {
 	for {
 		buf := make([]byte, 512)
 		l, err := c.Read(buf)
+		if err == io.EOF {
+			return
+		}
 		if err != nil {
 			log.Println(err)
-			if err==io.EOF{
-				return
-			}
+
 		}
 		log.Println(string(buf[:l]))
-		_,err=io.WriteString(c, route.DoReply(string(buf[:l]))+"\n")
+		_, err = io.WriteString(c, route.DoReply(string(buf[:l]))+"\n")
 		if err != nil {
 			panic("socket error")
 		}
@@ -49,8 +50,4 @@ func resolve_conn(c net.Conn){
 	}
 }
 
-func parsePars(msg []byte)[]string{
-	msgStr:=string(msg)
-	return strings.Split(msgStr,"\r\n")
 
-}
--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,38 +1,37 @@
 package storage
 
 import (
-	"fmt"
+	"log"
 	"testing"
 )
 
-func TestSet(t *testing.T) {
-	s,err:=GetResult("set","liufei","niubi")
-	if err != nil {
-		t.Error(err)
-	}else {
-		fmt.Println(s)
-	}
+var tt [][]string = [][]string{
+	{"get", "liufei"},
+	{"set", "liufei", "liufei"},
+	{"get", "liufei"},
+	{"del", "liufei"},
+	{"get", "liufei"},
 }
 
-func TestDel(t *testing.T) {
-	s,err:=GetResult("del","liufei")
-	if err != nil {
-		t.Fatal(err)
-	}else {
-		fmt.Println(s)
-	}
-}
-func TestGet(t *testing.T) {
-	s,err:=GetResult("get","liufei")
-	if err != nil {
-		fmt.Println(s)
-	}else {
-		fmt.Println(s)
+func TestDojob(t *testing.T) {
+	for _, s := range tt {
+		r, err := GetResult(s...)
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println(r)
+		}
 	}
 }
 func BenchmarkRun(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		GetResult("set","liufei","niubi")
-		GetResult("get","liufei")
+		for _, s := range tt {
+			r, err := GetResult(s...)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println(r)
+			}
+		}
 	}
 }
