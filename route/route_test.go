6b7321f64f9d61b7e72c5f60f6db0da6c3a5fae9
liufei <1373945731liufei@gmail.com>
2019-03-21 12:14:36 +0800

    route test

--- route/route_test.go
+++ route/route_test.go
@@ -1,1 +1,31 @@
 package route
+
+import "testing"
+
+type request_response struct {
+	request, response string
+}
+
+var tt []request_response = []request_response{
+	{"get liufei", "not exist"},
+	{"set liufei hello", "ok"},
+	{"get liufei", "hello"},
+	{"del liufei", "delete success"},
+	{"get liufei", "not exist"},
+}
+
+func TestDojob(t *testing.T) {
+	for _, s := range tt {
+		r := DoReply(s.request)
+		if r != s.response {
+			t.Fatal(s, r)
+		}
+	}
+}
+func BenchmarkRun(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, s := range tt {
+			_ = DoReply(s.request)
+		}
+	}
+}
