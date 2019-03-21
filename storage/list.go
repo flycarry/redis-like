bb40fd95d587c91f0a36665ac765b8874459d8e8
zhengyu02 <zhengyu02@inspur.com>
2019-03-21 22:21:49 +0800

    Extract the public part of the function about list

--- storage/list.go
+++ storage/list.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func init() {
@@ -29,49 +30,26 @@ func Lpush(args []string) (string, error) {
 		return "", ErrInvalidNumPara
 	}
 	strKey := args[0]
-	DataLock.RLock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.RUnlock()
-		tempList, err := tryTransform(value.Princess)
+	tempList, tempLock, err := readTableHelper(strKey, false)
+	if err == ErrKeyNotExist {
+		tempList, tempLock, err = writeTableHelper(strKey)
 		if err != nil {
 			return "", err
 		}
 		for _, arg := range args[1:] {
 			tempList.PushFront(arg)
 		}
-		value.Lock.Unlock()
+		tempLock.Unlock()
 		return "+OK", nil
+
 	}
-	DataLock.RUnlock()
-	DataLock.Lock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.Unlock()
-		tempList, err := tryTransform(value.Princess)
-		if err != nil {
-			return "", err
-		}
-		for _, arg := range args[1:] {
-			tempList.PushFront(arg)
-		}
-		value.Lock.Unlock()
-		return "+OK", nil
-	}
-	value := &Value{
-		Princess: newStrongList(),
-	}
-	value.Lock.Lock()
-	BigData[strKey] = value
-	DataLock.Unlock()
-	tempList, err := tryTransform(value.Princess)
 	if err != nil {
 		return "", err
 	}
 	for _, arg := range args[1:] {
 		tempList.PushFront(arg)
 	}
-	value.Lock.Unlock()
+	tempLock.Unlock()
 	return "+OK", nil
 }
 
@@ -81,49 +59,26 @@ func Rpush(args []string) (string, error) {
 		return "", ErrInvalidNumPara
 	}
 	strKey := args[0]
-	DataLock.RLock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.RUnlock()
-		tempList, err := tryTransform(value.Princess)
+	tempList, tempLock, err := readTableHelper(strKey, false)
+	if err == ErrKeyNotExist {
+		tempList, tempLock, err = writeTableHelper(strKey)
 		if err != nil {
 			return "", err
 		}
 		for _, arg := range args[1:] {
 			tempList.PushBack(arg)
 		}
-		value.Lock.Unlock()
+		tempLock.Unlock()
 		return "+OK", nil
+
 	}
-	DataLock.RUnlock()
-	DataLock.Lock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.Unlock()
-		tempList, err := tryTransform(value.Princess)
-		if err != nil {
-			return "", err
-		}
-		for _, arg := range args[1:] {
-			tempList.PushBack(arg)
-		}
-		value.Lock.Unlock()
-		return "+OK", nil
-	}
-	value := &Value{
-		Princess: newStrongList(),
-	}
-	value.Lock.Lock()
-	BigData[strKey] = value
-	DataLock.Unlock()
-	tempList, err := tryTransform(value.Princess)
 	if err != nil {
 		return "", err
 	}
 	for _, arg := range args[1:] {
 		tempList.PushBack(arg)
 	}
-	value.Lock.Unlock()
+	tempLock.Unlock()
 	return "+OK", nil
 }
 
@@ -133,23 +88,16 @@ func Lpop(args []string) (result string, err error) {
 		return "", ErrInvalidNumPara
 	}
 	strKey := args[0]
-	DataLock.RLock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.RUnlock()
-		tempList, err := tryTransform(value.Princess)
-		if err != nil {
-			return "", err
-		}
-		if tempList.Len() == 0 {
-			value.Lock.Unlock()
-			return "", ErrKeyNotExist
-		}
-		result = tempList.Remove(tempList.Front()).(string)
-		value.Lock.Unlock()
-	} else {
-		DataLock.RUnlock()
+	tempList, tempLock, err := readTableHelper(strKey, false)
+	if err != nil {
+		return
 	}
+	if tempList.Len() == 0 {
+		err = ErrKeyNotExist
+	} else {
+		result = tempList.Remove(tempList.Front()).(string)
+	}
+	tempLock.Unlock()
 	return
 }
 
@@ -159,23 +107,16 @@ func Rpop(args []string) (result string, err error) {
 		return "", ErrInvalidNumPara
 	}
 	strKey := args[0]
-	DataLock.RLock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.Lock()
-		DataLock.RUnlock()
-		tempList, err := tryTransform(value.Princess)
-		if err != nil {
-			return "", err
-		}
-		if tempList.Len() == 0 {
-			value.Lock.Unlock()
-			return "", ErrKeyNotExist
-		}
-		result = tempList.Remove(tempList.Back()).(string)
-		value.Lock.Unlock()
-	} else {
-		DataLock.RUnlock()
+	tempList, tempLock, err := readTableHelper(strKey, false)
+	if err != nil {
+		return
 	}
+	if tempList.Len() == 0 {
+		err = ErrKeyNotExist
+	} else {
+		result = tempList.Remove(tempList.Back()).(string)
+	}
+	tempLock.Unlock()
 	return
 }
 
@@ -185,55 +126,53 @@ func Lrange(args []string) (string, error) {
 	if len(args) != 3 {
 		return "", ErrInvalidNumPara
 	}
-	start, err := ifstrwmod(args[1], len(args))
+
+	// check if parameters is legal
+	start, err := intFromStr(args[1], len(args))
 	if err != nil {
 		return "", err
 	}
-	end, err := ifstrwmod(args[2], len(args))
+	end, err := intFromStr(args[2], len(args))
 	if err != nil {
 		return "", err
 	}
 	if start >= end {
 		return "(empty list)", nil
 	}
-	strKey := args[0]
-	DataLock.RLock()
-	if value, ok := BigData[strKey]; ok {
-		value.Lock.RLock()
-		DataLock.RUnlock()
-		tempList, err := tryTransform(value.Princess)
-		if err != nil {
-			return "", err
-		}
-		if tempList.Len() == 0 {
-			value.Lock.RUnlock()
-			return "", ErrKeyNotExist
-		}
-		tempArray := make([]string, 0, tempList.Len())
-		index := 0
-	Loop:
-		for tempEle := tempList.Front(); tempEle != nil; tempEle = tempEle.Next() {
-			switch {
-			case index < start:
-				index++
-			case index >= start && index < end:
-				tempArray = append(tempArray, fmt.Sprintf("%d) %s", index+1, tempEle.Value.(string)))
-				index++
-			default:
-				break Loop
-			}
-		}
 
-		if len(tempArray) == 0 {
-			return "(empty list)", nil
-		}
-		return strings.Join(tempArray, "\r\n"), nil
+	strKey := args[0]
+	tempList, tempLock, err := readTableHelper(strKey, true)
+	if err != nil {
+		return "", err
 	}
-	DataLock.RUnlock()
-	return "", ErrKeyNotExist
+	if tempList.Len() == 0 {
+		tempLock.RUnlock()
+		return "", ErrKeyNotExist
+	}
+
+	// the list is exist
+	tempArray := make([]string, 0, tempList.Len())
+	index := 0
+Loop:
+	for tempEle := tempList.Front(); tempEle != nil; tempEle = tempEle.Next() {
+		switch {
+		case index < start:
+			index++
+		case index >= start && index < end:
+			tempArray = append(tempArray, fmt.Sprintf("%d) %s", index+1, tempEle.Value.(string)))
+			index++
+		default:
+			break Loop
+		}
+	}
+
+	if len(tempArray) == 0 {
+		return "(empty list)", nil
+	}
+	return strings.Join(tempArray, "\r\n"), nil
 }
 
-func ifstrwmod(index string, len int) (ret int, err error) {
+func intFromStr(index string, len int) (ret int, err error) {
 	ret, err = strconv.Atoi(index)
 	if err != nil {
 		return 0, ErrInvalidPara
@@ -248,6 +187,48 @@ func tryTransform(value interface{}) (ret *strongList, err error) {
 	ret, ok := value.(*strongList)
 	if !ok {
 		return nil, ErrMismatchStruct
+	}
+	return
+}
+
+// writeTableHelper help function Lpush/Rpush to write, and caller should call lock's Lock()
+func writeTableHelper(strKey string) (tempList *strongList, lock *sync.RWMutex, err error) {
+	DataLock.Lock()
+	if value, ok := BigData[strKey]; ok {
+		lock = &value.Lock
+		lock.Lock()
+		DataLock.Unlock()
+		tempList, err = tryTransform(value.Princess)
+	} else {
+		value := &Value{
+			Princess: newStrongList(),
+		}
+		lock = &value.Lock
+		lock.Lock()
+		BigData[strKey] = value
+		DataLock.Unlock()
+		tempList, err = tryTransform(value.Princess)
+	}
+	return
+}
+
+// readTableHelper check the key is exist or not, if not `err` will be ErrKeyNotExist
+// else return tempList and caller should call lock's RUlock()/Ulock by read
+func readTableHelper(strKey string, read bool) (tempList *strongList, lock *sync.RWMutex, err error) {
+	DataLock.RLock()
+	if value, ok := BigData[strKey]; ok {
+		lock = &value.Lock
+		if read {
+			lock.RLock()
+		} else {
+			lock.Lock()
+		}
+		DataLock.RUnlock()
+		tempList, err = tryTransform(value.Princess)
+		// transfer succeed
+	} else {
+		DataLock.RUnlock()
+		err = ErrKeyNotExist
 	}
 	return
 }
