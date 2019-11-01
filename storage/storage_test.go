1446b09123868a2762183aec33a446eaf889f4df
liufei <liufei@sensetime.com>
2019-11-01 14:52:54 +0800

    修复并发问题，目前对于map和value的读写锁设计有问题

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	concurrent = 200
+	concurrent = 20
 )
 
 type params_result struct {
@@ -17,34 +17,34 @@ type params_result struct {
 }
 
 var tt []params_result = []params_result{
-	//{[]string{"get", "hello"}, "not exist"},
+	{[]string{"get", "hello"}, "not exist"},
 	{[]string{"set", "hello", "nihao"}, "ok"},
-	//{[]string{"get", "hello"}, "nihao"},
-	//{[]string{"del", "hello"}, "delete success"},
-	//{[]string{"get", "hello"}, "not exist"},
-	//{[]string{"lpush", "hello", "world"}, "1"},
-	//{[]string{"lpop", "hello"}, "world"},
-	//{[]string{"lpop", "hello"}, "not exist"},
-	//{[]string{"rpush", "hello", "world"}, "1"},
-	//{[]string{"rpop", "hello"}, "world"},
-	//{[]string{"rpop", "hello"}, "not exist"},
-	//{[]string{"lpush", "hello", "world"}, "1"},
-	//{[]string{"rpush", "hello", "test"}, "2"},
-	//{[]string{"lrange", "hello", "0","2"}, "world\ntest"},
-	//{[]string{"lrange", "hello", "0","1"}, "world"},
-	//{[]string{"lrange", "hello", "1","2"}, "test"},
-	//{[]string{"lrange", "hello", "-2","2"}, "world\ntest"},
-	//{[]string{"lrange", "hello", "-2","-1"}, "world"},
-	//{[]string{"lrange", "hello", "-2","-2"}, ""},
-	//{[]string{"lpop", "hello"}, "world"},
-	//{[]string{"lindex", "hello","1"}, "test"},
-	//{[]string{"linsert", "hello","test","nihao"}, "ok"},
-	//{[]string{"lindex", "hello","1"}, "nihao"},
-	//{[]string{"rpop", "hello"}, "test"},
-	//{[]string{"lindex", "hello","2"}, "error index"},
-	//{[]string{"lpop", "hello"}, "nihao"},
-	//{[]string{"rpop", "hello"}, "not exist"},
-	//{[]string{"lpop", "hello"}, "not exist"},
+	{[]string{"get", "hello"}, "nihao"},
+	{[]string{"del", "hello"}, "delete success"},
+	{[]string{"get", "hello"}, "not exist"},
+	{[]string{"lpush", "hello", "world"}, "1"},
+	{[]string{"lpop", "hello"}, "world"},
+	{[]string{"lpop", "hello"}, "not exist"},
+	{[]string{"rpush", "hello", "world"}, "1"},
+	{[]string{"rpop", "hello"}, "world"},
+	{[]string{"rpop", "hello"}, "not exist"},
+	{[]string{"lpush", "hello", "world"}, "1"},
+	{[]string{"rpush", "hello", "test"}, "2"},
+	{[]string{"lrange", "hello", "0", "2"}, "world\ntest"},
+	{[]string{"lrange", "hello", "0", "1"}, "world"},
+	{[]string{"lrange", "hello", "1", "2"}, "test"},
+	{[]string{"lrange", "hello", "-2", "2"}, "world\ntest"},
+	{[]string{"lrange", "hello", "-2", "-1"}, "world"},
+	{[]string{"lrange", "hello", "-2", "-2"}, ""},
+	{[]string{"lpop", "hello"}, "world"},
+	{[]string{"lindex", "hello", "1"}, "test"},
+	{[]string{"linsert", "hello", "test", "nihao"}, "ok"},
+	{[]string{"lindex", "hello", "1"}, "nihao"},
+	{[]string{"rpop", "hello"}, "test"},
+	{[]string{"lindex", "hello", "2"}, "error index"},
+	{[]string{"lpop", "hello"}, "nihao"},
+	{[]string{"rpop", "hello"}, "not exist"},
+	{[]string{"lpop", "hello"}, "not exist"},
 }
 
 func TestDoJob(t *testing.T) {
@@ -69,7 +69,6 @@ func TestDoJob(t *testing.T) {
 func TestConcurrent(t *testing.T) {
 	wg := sync.WaitGroup{}
 	wg.Add(concurrent)
-	m := 0
 	for i := 0; i < concurrent; i++ {
 		go func(i int) {
 			for _, s := range tt {
@@ -85,8 +84,6 @@ func TestConcurrent(t *testing.T) {
 					}
 				}
 			}
-			m++
-			log.Println(m)
 			wg.Done()
 		}(i)
 	}
